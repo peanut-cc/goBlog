0ccf6fb91730ba413fea211fcf556bd7afb88e5f
fan.zhao <hjzhaofan@gmail.com>
2020-09-07 23:38:10 +0800

    feat(internal/app/controller/api.go): add update password api

--- internal/app/controller/api.go
+++ internal/app/controller/api.go
@@ -8,7 +8,9 @@ import (
 	"github.com/peanut-cc/goBlog/internal/app/config"
 	"github.com/peanut-cc/goBlog/internal/app/ent/user"
 	"github.com/peanut-cc/goBlog/internal/app/global"
+	"github.com/peanut-cc/goBlog/internal/app/iutils"
 	"github.com/peanut-cc/goBlog/pkg/logger"
+	"github.com/peanut-cc/goBlog/pkg/utils"
 )
 
 const (
@@ -27,6 +29,7 @@ func init() {
 	// 更新帐号信息
 	APIs["account"] = apiAccount
 	APIs["blog"] = apiBlog
+	APIs["password"] = apiPassword
 }
 
 func apiAccount(c *gin.Context) {
@@ -75,6 +78,34 @@ func apiBlog(c *gin.Context) {
 		return
 	}
 	responseNotice(c, NOTICE_NOTICE, "更新成功", "")
+}
+
+func apiPassword(c *gin.Context) {
+	old := c.PostForm("old")
+	new := c.PostForm("new")
+	confirm := c.PostForm("confirm")
+
+	if new != confirm {
+		responseNotice(c, NOTICE_NOTICE, "两次密码输入不一致", "")
+		return
+	}
+	admin, err := global.EntClient.User.Query().Where(user.UsernameEQ(config.C.Blog.UserName)).Only(c)
+	if err != nil {
+		responseNotice(c, NOTICE_NOTICE, "未找到用户", "")
+		return
+	}
+	if !iutils.VerifyPasswd(admin.Password, admin.Username, old) {
+		responseNotice(c, NOTICE_NOTICE, "原始密码不正确", "")
+		return
+	}
+	newPwd := utils.EncryptPasswd(admin.Username, new)
+	_, err = admin.Update().SetPassword(newPwd).Save(c)
+	if err != nil {
+		logger.StartSpan(c, logger.SetSpanFuncName("apiPassword")).Errorf("admin update password error:%v", err.Error())
+		responseNotice(c, NOTICE_NOTICE, err.Error(), "")
+		return
+	}
+	responseNotice(c, NOTICE_SUCCESS, "更新成功", "")
 }
 
 func responseNotice(c *gin.Context, typ, content, hl string) {
